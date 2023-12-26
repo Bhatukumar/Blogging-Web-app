7a0b10e477dc98add6af1fd943d522be0d3c362d
manmay27 <manmay2002@gmail.com>
2023-12-26 15:36:41 +0530

    frontend with routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,35 @@ import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-
+import { BrowserRouter as Router, Route, Switch,Link} from "react-router-dom";
+// Ensure you're using the correct import syntax without an alias for Switch
 
 function App() {
-  return (
-    <>
-  <TopBar/>
- <Register/>
 
-    </>
+  const currentUser = true;
+  return (
+   <Router>
+    <TopBar/>
+    <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/posts">
+          <Home />
+        </Route>
+        <Route path="/register">
+          {currentUser ? <Home /> : <Register />}
+        </Route>
+        <Route path="/login">{currentUser ? <Home /> : <Login />}</Route>
+        <Route path="/post/:id">
+          <Single />
+        </Route>
+        <Route path="/write">{currentUser ? <Write /> : <Login />}</Route>
+        <Route path="/settings">
+          {currentUser ? <Settings /> : <Login />}
+        </Route>
+      </Switch>
+   </Router>
   
   );
 }
--- frontend/src/components/topbar/TopBar.jsx
+++ frontend/src/components/topbar/TopBar.jsx
@@ -1,8 +1,9 @@
 
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import "./topbar.css";
 
 export default function Topbar() {
-  
+  const user=true; 
   return (
     <div className="top">
       <div className="topLeft">
@@ -14,39 +15,54 @@ export default function Topbar() {
       <div className="topCenter">
         <ul className="topList">
           <li className="topListItem">
-           
-              HOME
+           <Link to="/" >
+           HOME
+           </Link>
+             
           
           </li>
-          <li className="topListItem">ABOUT</li>
-          <li className="topListItem">CONTACT</li>
+          <li className="topListItem"><Link className="link" to="/">ABOUT</Link></li>
+          <li className="topListItem"><Link className="link" to="/contact">CONTACT</Link></li>
           <li className="topListItem">
-           
-              WRITE
-           
+          <Link className="link" to="/write">WRITE</Link>
+          </li>
+          <li className="topListItem">
+          {user && "LOGOUT"}
+          
           </li>
           
         </ul>
       </div>
       <div className="topRight">
+        {
+          user? (
         
             <img
               className="topImg"
               src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
               alt=""
             />
-          
+          ) :(
+            <>
+            <Link className="link" to="/login">LOGIN</Link>
+            <Link className="link" to="/register">REGISTER</Link>
+            </>
+           
+          )
+        }
         
           <ul className="topList">
             <li className="topListItem">
-            
-                LOGIN
+            <Link className="link" to="/login">
+            LOGIN
+            </Link>
+               
              
             </li>
             <li className="topListItem">
-              
-                REGISTER
-             
+            <Link className="link" to="/register">
+              REGISTER
+            </Link>
             </li>
           </ul>
        
